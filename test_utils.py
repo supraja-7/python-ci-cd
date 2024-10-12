5d8f7e07f9fc76079bfa460247f33d254fa04b08
Nadeem Sajjad <nady5730642@gmail.com>
2024-10-12 06:56:38 +0000

    unit tests

---
+++ test_utils.py
@@ -0,0 +1,25 @@
+import unittest
+from utils import is_even_and_positive
+
+class TestUtils(unittest.TestCase):
+
+    def test_1(self):
+        result = is_even_and_positive(7)
+        self.assertEqual(result, False)
+    def test_2(self):
+        result = is_even_and_positive(10)
+        self.assertEqual(result, True)
+    def test_3(self):
+        result = is_even_and_positive(0)
+        self.assertEqual(result, False)
+    def test_4(self):
+        result = is_even_and_positive(-3)
+        self.assertEqual(result, False)
+    def test_5(self):
+        result = is_even_and_positive(-4)
+        self.assertEqual(result, False)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
