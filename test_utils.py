9571707000f89021569c72c471df24c3342a677a
bhuvanesh1440 <bhuvanesh1440@gmail.com>
2024-10-13 10:30:01 +0530

    added a new test case tes_11

--- test_utils.py
+++ test_utils.py
@@ -21,6 +21,9 @@ class TestUtils(unittest.TestCase):
     def test_11(self):
         result = is_even_and_positive(-21)
         self.assertEqual(result, False)
+    def test_11(self):
+        result = is_even_and_positive(1)
+        self.assertEqual(result, True)
 
 
 
